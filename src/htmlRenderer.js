6144e74447f82ba212de4f0317539899ea354e91
Cong Thanh Pham <chrispham211@gmail.com>
2023-01-28 03:25:53 -0600

    Added README

--- src/htmlRenderer.js
+++ src/htmlRenderer.js
@@ -7,7 +7,7 @@ function htmlRenderer(data) {
       <meta charset="UTF-8">
       <meta http-equiv="X-UA-Compatible" content="IE=edge">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <title>My Team</title>
+      <title>Team Profile</title>
 
       <!-- Bootstrap -->
       <link
